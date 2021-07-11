529bc362c091dca4757ae9d635f1b497f76203e0
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-11 14:40:59 -0500

    [Add]: Sliver Poster & Title

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({ Key? key }) : super(key: key);
+  const DetailsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     // TODO: Cambiar por una instancia de movie
-    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
 
     return Scaffold(
       // appBar: AppBar(
@@ -20,7 +20,10 @@ class DetailsScreen extends StatelessWidget {
       // ),
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([_PosterAndTitle()]),
+          ),
         ],
       ),
     );
@@ -28,7 +31,7 @@ class DetailsScreen extends StatelessWidget {
 }
 
 class _CustomAppBar extends StatelessWidget {
-  const _CustomAppBar({ Key? key }) : super(key: key);
+  const _CustomAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +47,7 @@ class _CustomAppBar extends StatelessWidget {
           width: double.infinity,
           alignment: Alignment.bottomCenter,
           child: Text(
-            'movie.title', 
+            'movie.title',
             style: TextStyle(fontSize: 16),
           ),
         ),
@@ -54,6 +57,60 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://via.placeholder.com/500x300'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star_outline,
+                  size: 15, 
+                  color: Colors.grey),
+                  SizedBox(width: 5,),
+                  Text('movie.voteAverage',
+                  style: textTheme.caption)
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
