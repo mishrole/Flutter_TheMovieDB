bf25ab91442be9a88b930b53d459c1602bb6ddfa
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-11 14:21:21 -0500

    [Add]: SliverAppBar on Details Screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,15 +5,56 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // TODO: Cambiar por una instancia de movie
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Details Page'),
+      // appBar: AppBar(
+      //   title: Text('Details Page'),
+      // ),
+      // body: Container(
+      //   child: Center(
+      //     child: Text('$movie'),
+      //   )
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
       ),
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
-        )
-      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.green,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          color: Colors.black12,
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            'movie.title', 
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        centerTitle: true,
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
